7d1aabba9c9a94e3220060ade87fcf7ed48b83a8
David Diers <david.diers@fourkitchens.com>
2017-02-26 13:21:01 -0600

    Cocktail changes to molotov.

--- test/molotovBadArgs.js
+++ test/molotovBadArgs.js
@@ -2,37 +2,44 @@ import test from 'ava';
 
 const Molotov = require('../molotov');
 
+// no args.
+test('molotovNoArgs', (t) => {
+  t.throws(() => {
+    const molotov = new Molotov();
+  });
+});
+
 // Test for bad path rejection.
 test('molotovBadPath', (t) => {
   t.throws(() => {
-    Molotov([], {}, {});
+    const molotov = new Molotov([], {}, {});
   });
 });
 
 // Test for bad supers rejection.
 test('molotovSupers', (t) => {
   t.throws(() => {
-    Molotov('./test/helpers/baba', {}, {});
+    const molotov = new Molotov('./test/helpers/baba', {}, {});
   });
 });
 
 // Test for bad supers rejection.
 test('molotovSupersNotObject', (t) => {
   t.throws(() => {
-    Molotov('./test/helpers/baba', 'not object', {});
+    const molotov = new Molotov('./test/helpers/baba', 'not object', {});
   });
 });
 
 // Test for bad plugin rejection.
 test('molotovBadPlugins', (t) => {
   t.throws(() => {
-    Molotov('./test/helpers/baba', {item: 'thing'}, {});
+    const molotov = new Molotov('./test/helpers/baba', {item: 'thing'}, {});
   });
 });
 
 // Test for bad plugin type rejection.
 test('molotovPluginsNotObject', (t) => {
   t.throws(() => {
-    Molotov('./test/helpers/baba', {item: 'thing'}, 'not an object');
+    const molotov = new Molotov('./test/helpers/baba', {item: 'thing'}, 'not an object');
   });
 });
