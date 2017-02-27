cf9f75b3473095ce6750606822faef46b0f80ba6
David Diers <david.diers@fourkitchens.com>
2017-02-26 23:47:29 -0600

    Cocktail improvements.

--- cocktail.js
+++ cocktail.js
@@ -11,8 +11,12 @@ module.exports = class {
    * @params {obj} molotovProvider
    *   The Molotov class implemented by a molotov provider module.
    * @params {obj} optional cocktailPluginDefinitions
-   *   An object of plugin definitions. If not provided will be extracted from config.
+   *   An object of plugin definitions. If not provided will be extracted from config. Config must come from one of the two places.
    * @params {obj} optional modulesPluginDirectory
+   *   If the cocktail implementing module is providing its own plugins, please require and supply them here.
+   * @params {obj} optional modulesSupers
+   *   If the cocktail implementing module is providing its own supers please require them and supply them here.
+   *
    */
   constructor(molotovProvider, cocktailPluginDefinitions, modulesPluginDirectory, modulesSupers) {
     // Get their supers, and plugins.
@@ -31,37 +35,64 @@ module.exports = class {
       }
       this.setCocktailPluginDefinitions(config.molotov.cocktailPlugins);
     }
-    // if our supers - set our supers
+    // If we have cocktail provided supers set them.
     if (modulesSupers) {
       this.setCocktailSupers(modulesSupers);
     }
-    // merge supers and plugins
+    // If we have cocktail provided plugins set them.
     if (modulesPluginDirectory) {
       this.setCocktailPlugins(modulesPluginDirectory);
     }
-
+    // merge supers and plugins
     this.mergeSupers();
     this.mergePlugins();
   }
 
+  /**
+   * setCocktailPluginDefinitions
+   *
+   * @param {obj} cocktailPluginDefinitions
+   *    A plugin definition keyed by supers and then keyed by plugin name
+   *      with values of the mixin file names it uses.
+   */
   setCocktailPluginDefinitions(cocktailPluginDefinitions) {
     this.cocktailPluginDefinitions = cocktailPluginDefinitions;
   }
 
+  /**
+   * getCocktailPluginDefinitions.
+   *
+   * @returns {obj} pluginDefinitions.
+   */
   getCocktailPluginDefinitions() {
     return this.cocktailPluginDefinitions;
   }
 
+  /**
+   * mergeSupers
+   *   Merges molotov supers and cocktailSupers (if defined).
+   */
   mergeSupers() {
     const mergedSupers = _.merge(this.getMolotovSupers(), this.getCocktailSupers());
     this.setMergedSupers(mergedSupers);
   }
 
+  /**
+   * Sets the merged supers.
+   *
+   * @param {obj} mergedSupers
+   *   An object of merged supers.
+   */
   setMergedSupers(mergedSupers) {
     // Merge supers
     this.mergedSupers = mergedSupers;
   }
 
+  /**
+   * getMergedSupers
+   *
+   * @returns mergedSupers
+   */
   getMergedSupers() {
     return this.mergedSupers;
   }
--- test/molotovBadArgs.js
+++ test/molotovBadArgs.js
@@ -5,41 +5,41 @@ const Molotov = require('../molotov');
 // no args.
 test('molotovNoArgs', (t) => {
   t.throws(() => {
-    const molotov = new Molotov();
+    const molotov = new Molotov();  // eslint-disable-line no-unused-vars
   });
 });
 
 // Test for bad path rejection.
 test('molotovBadPath', (t) => {
   t.throws(() => {
-    const molotov = new Molotov([], {}, {});
+    const molotov = new Molotov([], {}, {}); // eslint-disable-line no-unused-vars
   });
 });
 
 // Test for bad supers rejection.
 test('molotovSupers', (t) => {
   t.throws(() => {
-    const molotov = new Molotov('./test/helpers/baba', {}, {});
+    const molotov = new Molotov('./test/helpers/baba', {}, {}); // eslint-disable-line no-unused-vars
   });
 });
 
 // Test for bad supers rejection.
 test('molotovSupersNotObject', (t) => {
   t.throws(() => {
-    const molotov = new Molotov('./test/helpers/baba', 'not object', {});
+    const molotov = new Molotov('./test/helpers/baba', 'not object', {}); // eslint-disable-line no-unused-vars
   });
 });
 
 // Test for bad plugin rejection.
 test('molotovBadPlugins', (t) => {
   t.throws(() => {
-    const molotov = new Molotov('./test/helpers/baba', {item: 'thing'}, {});
+    const molotov = new Molotov('./test/helpers/baba', {item: 'thing'}, {}); // eslint-disable-line no-unused-vars
   });
 });
 
 // Test for bad plugin type rejection.
 test('molotovPluginsNotObject', (t) => {
   t.throws(() => {
-    const molotov = new Molotov('./test/helpers/baba', {item: 'thing'}, 'not an object');
+    const molotov = new Molotov('./test/helpers/baba', {item: 'thing'}, 'not an object'); // eslint-disable-line no-unused-vars
   });
 });
