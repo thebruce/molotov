fdf96ac47341dfb954ba6d96b91fd798b1fa394c
David Diers <david.diers@fourkitchens.com>
2018-03-01 22:55:14 -0600

    fix(jest): Removed extra old ava tests.

--- __tests__/superMixologistNoConfig.js
+++
@@ -1,9 +0,0 @@
-const MolotovProviderBase = require('../molotovProviderBase');
-
-// Tests for if the user is not using the config module and provides no config.
-test('setOverrides', async () => {
-  const superMixologist = new MolotovProviderBase('./test/helpers/');
-  superMixologist.setOverrides({ superKeyName: 'thing' });
-  t.context.data = await superMixologist.getOverrides();
-  expect(t.context.data).toEqual({ superKeyName: 'thing' });
-});
