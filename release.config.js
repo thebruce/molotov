711fa2e0af523a5b9d9dfc1b85bd3672a0d42102
David Diers <david.diers@fourkitchens.com>
2019-02-26 14:18:53 -0600

    fix(github): fix github release

--- release.config.js
+++ release.config.js
@@ -3,22 +3,6 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
-    [
-      '@semantic-release/github',
-      {
-        assets: [
-          { path: 'lib/**', label: 'Library' },
-          { path: 'examples/**', label: 'Examples' },
-          { path: 'flow-typed/**', label: 'Flow Typed' },
-          { path: 'schema/**', label: 'Molotov Schema' },
-          { path: '_errors.js', label: 'Errors' },
-          { path: 'yarn.lock', label: 'Yarn Lock' },
-          { path: 'README.md', label: 'Read Me' },
-          { path: 'package.json', label: 'Examples' },
-          { path: 'LICENSE', label: 'License' },
-          { path: 'index.js', label: 'Index' },
-        ],
-      },
-    ],
+    '@semantic-release/github',
   ],
 };
